a12b8fdda64f378e0ddc960ea2a02f65cc457785
Igor Chechura <igorchechura@gmail.com>
2022-09-20 10:40:09 +0300

    fonts, header

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 
 mix.copyDirectory('src/img', 'assets/img');
+mix.copyDirectory('src/fonts', 'assets/fonts');
 
 mix.js('src/js/main.js', 'assets/js/').sass('src/sass/main.scss', 'assets/css/').options({
     processCssUrls: false
